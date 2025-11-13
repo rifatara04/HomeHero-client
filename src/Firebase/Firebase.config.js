0a684c5d6c9d6acde2ebfe47f6bb4b0448fde8f7
rifatara04 <your_email@example.com>
2025-11-13 21:10:04 +0600

    some changes

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,7 +5,15 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCw2PXaOddbt1HgZVoEnqynCCg0dT6ZY8A",
+  authDomain: "homehero-2a2cf.firebaseapp.com",
+  projectId: "homehero-2a2cf",
+  storageBucket: "homehero-2a2cf.firebasestorage.app",
+  messagingSenderId: "829149508479",
+  appId: "1:829149508479:web:95066dd5252a8f99fe604a",
+  measurementId: "G-PBGN2QRG9W"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
